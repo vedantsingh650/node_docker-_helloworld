7f21998ee5aa25a9a4d006a764bffa0db17d08f8
vedant <vedantsingh650@gmail.com>
2021-05-14 19:34:14 +0530

    node

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const express = require("express");
+
+// Constants
+const port = 9090;
+//const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
